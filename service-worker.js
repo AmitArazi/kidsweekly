d7e1a488d5c0eac5013a25438f26daff45a431c8
AmitArazi <41228410+AmitArazi@users.noreply.github.com>
2026-08-02 23:05:05 +0300

    Refresh PWA cache for babysitter scheduling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kidsweekly-v6";
+const CACHE_NAME = "kidsweekly-v7";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
